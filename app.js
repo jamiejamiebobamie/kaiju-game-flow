1689ab40e469cd256fb7de5176f681a99536e041
jamiejamiebobamie <jmccrory@vt.edu>
2022-01-23 17:41:46 -0500

    deploying to heroku.

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// app.listen(3000);
+app.listen(PORT);
